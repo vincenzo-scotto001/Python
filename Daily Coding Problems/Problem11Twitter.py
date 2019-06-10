4003dea39809bf7ee8e54380846bf4a94a352a2a
vincenzo-scotto001 <vincenzo.scotto001@gmail.com>
2019-06-10 13:22:36 -0400

    solved problem 11

--- Daily Coding Problems/Problem11Twitter.py
+++ Daily Coding Problems/Problem11Twitter.py
@@ -5,3 +5,21 @@
 # For example, 
 # given the query string de and the set of strings [dog, deer, deal], return [deer, deal].
 
+
+test = ['dog', 'deer', 'deal'] #create the sample list of strings
+query = 'de' # create the queue
+twitter = list(query.split(' ')) #convert to list for convience
+matches = [] #create empty list
+
+
+#takes in 3 parameters , looks at the queue and sees if the len of the queue matches with any of the strings and appends them in the list called matches
+def twitters(test,twitter,matches):
+    for c in test:
+        if c[0:len(twitter)+1] == twitter[0]:
+        #print(''.join(list(c[:])))
+            matches.append(''.join(list(c[:])))
+    return matches
+
+
+twitters(test,twitter,matches)
+
